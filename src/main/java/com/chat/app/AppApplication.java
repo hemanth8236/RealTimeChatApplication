bff54d5cb69e2267cfde7c55cebb6f4dde186fd3
hemanth p <hemanth9103@gmail.com>
2025-06-04 12:35:27 +0530

    basic comments

--- src/main/java/com/chat/app/AppApplication.java
+++ src/main/java/com/chat/app/AppApplication.java
@@ -2,7 +2,9 @@ package com.chat.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Main entry point for the Spring Boot Chat Application.
+ */
 @SpringBootApplication
 public class AppApplication {
 
